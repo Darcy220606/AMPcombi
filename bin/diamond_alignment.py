699f78b5327d70e1cc73059a8cbf5f9c4734761c
Anan Ibrahim <81744003+Darcy220606@users.noreply.github.com>
2022-09-28 00:37:02 +0200

    Delete diamond_alignment.py

--- bin/diamond_alignment.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-
-# TITLE: Diamond alignment to amp_database
-
-########################################
-#  Diamond alignment
-#########################################
-def diamond_alignment(db, out_path):
-    
