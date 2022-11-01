a75bc00c32ecf873a133b18cf01f172ad9cf0d2d
darcy220606 <ananhamido@hotmail.com>
2022-11-01 16:07:42 +0100

    Add the diamond threads as argument

--- ampcombi/diamond_makedb.sh
+++ ampcombi/diamond_makedb.sh
@@ -5,13 +5,16 @@
 #########################################
 
 INPUT_FASTA=$1
+THREADS=$2
 #OUTPUT_DIR=$2
 
 read INPUT_FASTA
+read THREADS
 #read OUTPUT_DIR
 
 IN=$INPUT_FASTA
+P=$THREADS
 #OUT=$OUTPUT_DIR 
 
 #cd $OUT
-diamond makedb --in $IN -p 28 -d amp_ref --quiet
+diamond makedb --in $IN -p $P -d amp_ref --quiet
